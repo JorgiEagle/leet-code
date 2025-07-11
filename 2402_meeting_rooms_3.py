4abe6880bb15f8750094242131e77cba0fc2c486
Jack Jorgensen <jackwjorgensen@gmail.com>
2025-07-11 12:48:28 +0100

    slow speed, better than average memory

--- 2402_meeting_rooms_3.py
+++ 2402_meeting_rooms_3.py
@@ -1,20 +1,23 @@
-from heapq import heappush, heappushpop
 from typing import List
 
 
 def mostBooked(n: int, meetings: List[List[int]]) -> int:
         # min heap of rooms. room meeting endtime, room number, number of meeting held in room
-        rooms = [(0, index, 0) for index in range(1, n)]
-        current_room = (0, 0, 0)
+        rooms = [(0, index, 0) for index in range(n)]
         meetings.sort(key=lambda x: x[0])
         for meeting_start, meeting_end in meetings:
             duration = meeting_end - meeting_start
-            room_endtime, room_number, number_of_meetings = current_room
+            current_room_index = 0
+            while current_room_index < len(rooms) and rooms[current_room_index][0] > meeting_start:
+                 current_room_index += 1
+            current_room_index = min(rooms, key=lambda x: x[0])[1] if current_room_index >= len(rooms) else current_room_index
+            room_endtime, room_number, number_of_meetings = rooms[current_room_index]
             room_endtime = max(room_endtime, meeting_start)
-            new_room = (room_endtime + duration, room_number, number_of_meetings + 1)
-            current_room = heappushpop(rooms, new_room)
-        heappush(rooms, current_room)
+            rooms[current_room_index] = (room_endtime + duration, room_number, number_of_meetings + 1)
+
         return max(rooms, key=lambda x: (x[2], -x[1]))[1]
+
+
 
 
 def main():
